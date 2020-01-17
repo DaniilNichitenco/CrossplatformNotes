c65cac614b4c9fa2eea531a421f04b60e735c766
Daniil Nichitenco <boxingchannel179@gmail.com>
2020-01-17 21:52:53 +0200

    Migrated to AndroidX

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'popup_content.dart';
 
 void main() => runApp(MyApp());
 Color colorTheme = Color.fromARGB(255, 33, 39, 97);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,75 +24,38 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-
   void createNote() {
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-   /* Widget searchBar = Container(
-        margin: EdgeInsets.only(
-            left: MediaQuery.of(context).size.width * 0.001, right: 0),
-        padding: EdgeInsets.zero,
-        height: MediaQuery.of(context).size.height * 0.045,
-        color: colorTheme,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(3.0)),
-          ),
-            child: TextField(
-              textInputAction: TextInputAction.search,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20
-              ),
-              decoration: InputDecoration(
-                filled: true,
-                contentPadding: EdgeInsets.only(
-                    //top: MediaQuery.of(context).size.height * 0.009,
-                  top: 0,
-                    left: MediaQuery.of(context).size.width * 0.03,
-                   bottom: 0
-                   // bottom: MediaQuery.of(context).size.height * 0.009
+    Widget searchBar = Container(
+      padding: EdgeInsets.only(left: 10),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: TextField(
+                textInputAction: TextInputAction.search,
+                style: TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Search",
+                  hintStyle: TextStyle(color: Color.fromARGB(255, 220, 220, 220)),
                 ),
-                border: InputBorder.none,
-                hintText: "Search",
               ),
             ),
           ),
-        ); */
-
-   Widget searchBar = Container(
-     margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
-     decoration: BoxDecoration(
-       color: Colors.white,
-       borderRadius: BorderRadius.all(Radius.circular(6.0)),
-     ),
-     child: Row(
-       crossAxisAlignment: CrossAxisAlignment.start,
-       children: <Widget>[
-         Expanded(
-           flex: 1,
-             child: TextFormField(
-               decoration: InputDecoration(
-                 border: InputBorder.none,
-                 hintText: "Search",
-               ),
-             )
-         ),
-       ],
-     ),
-   );
+        ],
+      ),
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -150,12 +114,12 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-      showPopup(context, _newNoteBody(), 'Creating new note');
-    },
-    tooltip: 'New note',
-    child: Icon(Icons.add),
+          showPopup(context, _newNoteBody(), 'Creating new note');
+        },
+        tooltip: 'New note',
+        child: Icon(Icons.add),
         backgroundColor: colorTheme,
-    ),
+      ),
     );
   }
 
@@ -194,14 +158,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _newNoteBody() {
-
     Widget inputName = Container(
-      color: Color.fromARGB(0, 149, 159, 223),
+        color: Color.fromARGB(0, 149, 159, 223),
         margin: EdgeInsets.only(
             left: MediaQuery.of(context).size.width * 0.12,
             right: MediaQuery.of(context).size.width * 0.12,
-            top: MediaQuery.of(context).size.height * 0.02
-        ),
+            top: MediaQuery.of(context).size.height * 0.02),
         padding: EdgeInsets.zero,
         height: MediaQuery.of(context).size.height * 0.045,
         child: DecoratedBox(
@@ -232,12 +194,19 @@ class _MyHomePageState extends State<MyHomePage> {
       color: Color.fromARGB(100, 149, 159, 223),
       padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
       child: Column(
-        children: <Widget>[Center(
-          child: Text("Name your note:", style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.height * 0.03), ),
-        ),
+        children: <Widget>[
+          Center(
+            child: Text(
+              "Name your note:",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.height * 0.03),
+            ),
+          ),
           inputName,
           Container(
-            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.15),
+            padding:
+            EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.15),
             child: Row(
               children: <Widget>[
                 FlatButton(
@@ -246,18 +215,26 @@ class _MyHomePageState extends State<MyHomePage> {
                         Navigator.pop(context); // close the popup
                       } catch (e) {}
                     },
-                    child: Text("Create", style: TextStyle(fontWeight: FontWeight.bold,
-                        fontSize: MediaQuery.of(context).size.height * 0.026, color: Colors.black54),)
-                ),
+                    child: Text(
+                      "Create",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.026,
+                          color: Colors.black54),
+                    )),
                 FlatButton(
                     onPressed: () {
                       try {
                         Navigator.pop(context); // close the popup
                       } catch (e) {}
                     },
-                    child: Text("Cancel", style: TextStyle(fontWeight: FontWeight.bold,
-                        fontSize: MediaQuery.of(context).size.height * 0.026, color: Colors.black54),)
-                ),
+                    child: Text(
+                      "Cancel",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.026,
+                          color: Colors.black54),
+                    )),
               ],
             ),
           ),
